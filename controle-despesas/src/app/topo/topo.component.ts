5f925fa26994e1c7da740bf663cdd820ebafabac
Maria do Céu <mariazinhasp@gmail.com>
2022-11-19 11:28:27 -0300

    Ajustes componentes

--- controle-despesas/src/app/topo/topo.component.ts
+++ controle-despesas/src/app/topo/topo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-topo',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./topo.component.css']
 })
 export class TopoComponent implements OnInit {
+  @ViewChild('mobile') sideNav?: ElementRef;
 
   constructor() { }
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    M.Sidenav.init(this.sideNav?.nativeElement);
+
+  }
+  
 
 }
